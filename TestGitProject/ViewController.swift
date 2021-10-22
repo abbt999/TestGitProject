3c5923eef8bfe4c5b3eb504978510efde72b086f
basit <abbt999@gmail.com>
2021-10-22 15:27:11 +0500

    Initial Commit

---
+++ TestGitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestGitProject
+//
+//  Created by apple on 10/22/21.
+//  Copyright © 2021 index. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
