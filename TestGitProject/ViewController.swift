f4157b8b623bf0fc9853b4e0129e4393494ef5d2
basit <abbt999@gmail.com>
2021-10-22 16:43:26 +0500

    images added

--- TestGitProject/ViewController.swift
+++ TestGitProject/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var image1: UIImageView!
+    @IBOutlet var image1Name: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
